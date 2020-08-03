2132a50964a31402e682927f978efb2642b6b5bd
kannannce1 <48784824+kannannce1@users.noreply.github.com>
2020-08-03 10:39:39 +0530

    updated add site

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,58 @@
-import * as React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
+import React, { Component } from "react";
+import {
+    Router,
+    Redirect,
+    Route,
+    Switch,
+} from "react-router-dom";
+import { observer, Provider } from "mobx-react";
+import history from "./history";
+import CommonHeader from './components/headercontainer/Header.component';
+import ModalComponent from './components/projectDetails/projDetails';
+import MapComponent from "./components/mapdetails/Map.component";
 
+import CoordinateDetail from './components/coordinatesDetails/CoordinateDetail';
+import SiteComponent from "./components/mapdetails/Site.component"
+
+import { ProjectDetailState } from './common/model/ProjectDetailsState';
+//import { ProjectStore } from './common/Store/newProjectStore';
+import { createStores } from './common/Store/CreateStore';
 import './custom.css'
 
-export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-    </Layout>
+
+
+
+const rootStore = createStores(
+    new ProjectDetailState()
 );
+
+@observer
+
+class App extends Component {
+    public render() {
+        return (
+
+            <Provider {...rootStore}>
+                <div>
+
+                    <Router history={history}>
+
+                        <Route exact component={CommonHeader} />
+
+                        <div id="content">
+                            <Switch>
+                                <Route path="/" exact component={ModalComponent} />
+
+                                <Route path="/map" component={MapComponent} />
+                                <Route path="/sidebar" component={SiteComponent} />
+
+                            </Switch>
+
+                        </div>
+                    </Router>
+                </div>
+            </Provider>
+        );
+    }
+}
+export default App;
